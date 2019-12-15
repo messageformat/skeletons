092530cbb8a5ec9e62f8701f6ebfe6d87f8f798d
Eemeli Aro <eemeli@gmail.com>
2019-12-14 23:21:35 -0500

    Copy build-readme script to date-skeleton

---
+++ packages/date-skeleton/scripts/build-readme.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const path = require('path')
+const { homepage, name } = require('../package.json')
+
+const footer = `\n\n---
+
+[Messageformat](https://messageformat.github.io/) is an OpenJS Foundation project, and we follow its [Code of Conduct](https://github.com/openjs-foundation/cross-project-council/blob/master/CODE_OF_CONDUCT.md).
+
+<a href="https://openjsf.org">
+<img width=200 alt="OpenJS Foundation" src="https://messageformat.github.io/messageformat/logo/openjsf.svg" />
+</a>`
+
+const bodySrcPath = path.resolve(
+  __dirname,
+  '../docs/messageformat-date-skeleton.md'
+)
+const bodySrc = fs.readFileSync(bodySrcPath, 'utf8')
+const headEnd = bodySrc.indexOf(`## ${name}`)
+
+const linkRe = new RegExp(`\\./${name}\\b`, 'g')
+const body = bodySrc
+  .slice(headEnd)
+  .replace(/^.*/, `# ${name}`)
+  .replace(linkRe, `${homepage}/blob/master/docs/${name}`)
+  .trim()
+
+const readmePath = path.resolve(__dirname, '../README.md')
+fs.writeFileSync(readmePath, body + footer)
